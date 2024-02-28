35a6e6353ae8d06922675e7aa80eb92076dd2fec
Anmol Upadhyay <anmolupadhau7@gmail.com>
2024-02-28 17:19:57 +0000

    deleted

--- 01_basics/04_comparison.js
+++
@@ -1,9 +0,0 @@
-console.log(null > 0)   // false
-console.log(null == 0)  // false
-console.log(null >= 0)  // true
-
-//  equality ==  and comparison  <,>,<=,>=  work differently, with comparison null becomes '0' that's why it is true
-
-// strict check  { === }
-
-console.log( "2" === 2 ) // return false because it also checks the datatype with value
