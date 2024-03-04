83fbdf26ae8e5f603337cf6b79dd0a5ac34ba0bf
Anmol Upadhyay <anmolupadhau7@gmail.com>
2024-03-04 19:02:10 +0000

    updating hoisting

--- 01_basics/07_hoisting.js
+++ 01_basics/07_hoisting.js
@@ -14,9 +14,19 @@ course()
 
 course() // will console chatgpt
 
-const y = 7;
+const y = 7
 
 function course(){
     console.log("Chatgpt")
 }
+
+// It's important to note that hoisting is specific to the declaration of variables and functions using var in JavaScript. Variables declared with let and const are hoisted as well, but they are not initialized until the actual code execution reaches their declaration.
+
+console.log(z); // ReferenceError: y is not defined 
+let z = 10;
+console.log(z); // 10
+
+
+// if you use arrow function to declare your function it will store it as a variable will assign undefine to it whereas if normal function it will give the entire code    [ go to sources in inspect and see using debugger ]
+
 
