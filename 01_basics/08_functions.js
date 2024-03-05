58412555ca81f4bbd6cfec7f39c0342c1cf43ebd
Anmol Upadhyay <anmolupadhau7@gmail.com>
2024-03-05 19:43:51 +0000

    adding 08

---
+++ 01_basics/08_functions.js
@@ -0,0 +1,26 @@
+// each and every function creates its local execution context 
+
+var x = 1;
+a();
+b();
+console.log(x);
+
+function a(){
+    var x = 10;
+    console.log(x);
+}
+
+function b(){
+    var x = 100;
+    console.log(x);
+}
+
+/*
+output 
+
+10
+100 
+1
+
+*/
+
