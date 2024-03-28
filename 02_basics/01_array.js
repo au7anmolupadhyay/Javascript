170a8ef5bc5b61a66faa4ea3bb589b53964f7ae8
Anmol Upadhyay <anmolupadhau7@gmail.com>
2024-03-28 17:09:13 +0000

    update

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -18,7 +18,7 @@ const myArr2 = new Array(1, 2, 3, 4)
 // console.log(myArr.includes(9));
 // console.log(myArr.indexOf(3));
 
-// const newArr = myArr.join()
+// const newArr = myArr.join()      changes type of array into string
 
 // console.log(myArr);
 // console.log( newArr);
@@ -28,12 +28,12 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 console.log("A ", myArr);
 
-const myn1 = myArr.slice(1, 3)
+const myn1 = myArr.slice(1, 3)          // does not manipulates the original array therefore only gives the range provided in (from, till) 
 
 console.log(myn1);
 console.log("B ", myArr);
 
 
-const myn2 = myArr.splice(1, 3)
+const myn2 = myArr.splice(1, 3)         //manipulates the original array therefore it extracts the range provided in the () 
 console.log("C ", myArr);
 console.log(myn2);
