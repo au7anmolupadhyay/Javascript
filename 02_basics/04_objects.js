ecc6105b980c7efa946a23f3d95877365ed18eab
Anmol Upadhyay <anmolupadhau7@gmail.com>
2024-03-29 18:31:41 +0000

    update

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -61,21 +61,3 @@ const course = {
     courseInstructor: "hitesh"
 }
 
-// course.courseInstructor
-
-const {courseInstructor: instructor} = course
-
-// console.log(courseInstructor);
-console.log(instructor);
-
-// {
-//     "name": "hitesh",
-//     "coursename": "js in hindi",
-//     "price": "free"
-// }
-
-[
-    {},
-    {},
-    {}
-]
