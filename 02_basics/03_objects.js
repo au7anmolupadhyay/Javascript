9d834262fa43718c183f5a4a922a1539bbde6e67
Anmol Upadhyay <anmolupadhau7@gmail.com>
2024-03-29 15:15:27 +0000

    update

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,42 @@
+// singleton
+// Object.create
+
+// object literals          literal ki trh banate hai jab object toh singleton nahi banta hai
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",      // if you want to use symbol in object you need to use [], else it will be of type string
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+console.log(JsUser["age"]);         // when using this way each and every property has to be invoked using inverted commas
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+// console.log(JsUser.greeting)          // this gives only the reference to the function
+// console.log(JsUser.greeting());
+// console.log(JsUser.greetingTwo());
+
