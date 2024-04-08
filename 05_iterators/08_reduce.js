ed67b6e53a4ff1dfc87eb6fc9b4d3a037ae80ea1
Anmol Upadhyay <anmolupadhau7@gmail.com>
2024-04-08 11:47:15 +0000

    update

--- 05_iterators/08_reduce.js
+++ 05_iterators/08_reduce.js
@@ -11,5 +11,26 @@ const myNums = [1,2,3]
 const myTotal = myNums.reduce((acc, curr) => acc + curr, 3
 )
 
+// console.log(myTotal);
 
-console.log(myTotal);
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const pay = shoppingCart.reduce((acc, item) => item.price + acc, 0)
+console.log(`total amount to pay: ${pay}`)
